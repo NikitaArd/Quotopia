4d1efa156b05bb7478a43c959b9f7499b51b3e66
NikitaArd <11nik11255@gmail.com>
2024-04-09 19:56:20 +0200

    feat: created header and uploaded some images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import QuestionMark from '../assets/question-mark.svg';
+
+export default function Header(){
+    return (
+        <header className="w-full h-24 bg-stone-400 flex justify-between px-36">
+            <h1 className="font-bold text-4xl my-auto">Quotopia</h1>
+            <button className="btn my-auto w-12 h-10 p-2 bg-stone-200 hover:bg-stone-100">
+                <img src={QuestionMark} className='' />
+            </button>
+        </header>
+    );
+}
