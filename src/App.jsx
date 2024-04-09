33fa3a21639a303a9ba3ccd1eab9b65040d9ad49
NikitaArd <11nik11255@gmail.com>
2024-04-09 21:35:16 +0200

    feat: created another component QuoteDisplay that displays quote, author, category of quote and regenerate button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import QuoteDisplay from "./components/QuoteDisplay";
 
 function App() {
   return (
     <>
       <Header />
+      <QuoteDisplay />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
         <header className="w-full h-24 bg-stone-400 flex justify-between px-36">
             <h1 className="font-bold text-4xl my-auto">Quotopia</h1>
             <button className="btn my-auto w-12 h-10 p-3 bg-stone-200 hover:bg-stone-100">
-                <img src={QuestionMark} className='' />
+                <img src={QuestionMark} />
             </button>
         </header>
     );
