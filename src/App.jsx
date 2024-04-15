a0fe82deefc716fa7f4c5a02ade9dfd140e95eee
NikitaArd <11nik11255@gmail.com>
2024-04-15 15:31:44 +0200

    feat: created getQuote from api function and display the result using useEffect hook, also created a Spinner while quote is loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import QuoteDisplay from "./components/QuoteDisplay";
+import Spinner from "./components/Spinner";
+
+const quoteAPI = "https://api.quotable.io/random";
+
 
 function App() {
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch(new Request(quoteAPI))
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Some errors !");
+        }
+  
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data)
+  
+        setQuote({
+          content: data.content,
+          author: data.author,
+          tags: data.tags,
+          error: "",
+        });
+      })
+      .catch((error) => {
+        setQuote({
+          content: '',
+          author: '',
+          tags: '',
+          error: error,
+        });
+      });
+  }, []);
+  
   return (
     <>
       <Header />
-      <QuoteDisplay />
+      { quote ? <QuoteDisplay quote={quote} /> : <Spinner />}
     </>
   );
 }
--- src/components/QuoteDisplay.jsx
+++ src/components/QuoteDisplay.jsx
@@ -1,15 +1,15 @@
 import GenerateAgain from '../assets/again.svg';
 
-export default function QuoteDisplay(){
+export default function QuoteDisplay({quote}){
     return (
         <>
 
         <section class="bg-white dark:bg-gray-900 mt-60">
             <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">If it is not right do not do it; if it is not true do not say it.</h1>
-                <p className="italic text-lg m-6 px-56 text-right">Marcus Aurelius</p>
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">{quote.content}</h1>
+                <p className="italic text-lg m-6 px-56 text-right">{ quote.author }</p>
                 <div className="m-auto h-auto mt-20">
-                    <span class="bg-gray-300 text-gray-800 text-m font-medium me-2 px-3.5 py-1.5 rounded-full dark:bg-gray-700 dark:text-gray-300">Famous Quotes</span>
+                    { quote.tags.reduce(tag => { return <span className="bg-gray-300 text-gray-800 text-m font-medium me-2 px-3.5 py-1.5 rounded-full dark:bg-gray-700 dark:text-gray-300">{tag}</span>}) }
                 </div>
             </div>
         </section>
