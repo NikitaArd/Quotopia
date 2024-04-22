cff90950613bee5a99a01370d5499dcaa52d63ea
Nikita Smolenskyi <57872055+NikitaArd@users.noreply.github.com>
2024-04-22 22:50:45 +0200

    feat/api request rendering (#3)
    * feat: created getQuote from api function and display the result using useEffect hook, also created a Spinner while quote is loading
    
    * fix: fixed quote category display
    
    * feat: added get new quote when button pressed (button pressed -> quote = null -> activating useEffect -> quote => {...} -> activating useEffect => ends becouse quote is not null)
    
    * fix: fixed less dangerous issues ( class -> className, added key prop into tag element )

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import QuoteDisplay from "./components/QuoteDisplay";
+import Spinner from "./components/Spinner";
+
+const quoteAPI = "https://api.quotable.io/random";
+
 
 function App() {
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    if(quote === null){
+      fetch(new Request(quoteAPI))
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Some errors !");
+        }
+  
+        return response.json();
+      })
+      .then((data) => {
+        setQuote({
+          content: data.content,
+          author: data.author,
+          tags: data.tags,
+          error: "",
+        });
+      })
+      .catch((error) => {
+        setQuote({
+          content: '',
+          author: '',
+          tags: '',
+          error: error,
+        });
+      });
+    }
+  }, [quote]);
+  
   return (
     <>
       <Header />
-      <QuoteDisplay />
+      { quote ? <QuoteDisplay quote={quote} handleSetQuote={setQuote} /> : <Spinner />}
     </>
   );
 }
--- src/components/QuoteDisplay.jsx
+++ src/components/QuoteDisplay.jsx
@@ -1,21 +1,21 @@
 import GenerateAgain from '../assets/again.svg';
 
-export default function QuoteDisplay(){
+export default function QuoteDisplay({quote, handleSetQuote}){
     return (
         <>
 
-        <section class="bg-white dark:bg-gray-900 mt-60">
-            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">If it is not right do not do it; if it is not true do not say it.</h1>
-                <p className="italic text-lg m-6 px-56 text-right">Marcus Aurelius</p>
+        <section className="bg-white dark:bg-gray-900 mt-60">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">{quote.content}</h1>
+                <p className="italic text-lg m-6 px-56 text-right">{ quote.author }</p>
                 <div className="m-auto h-auto mt-20">
-                    <span class="bg-gray-300 text-gray-800 text-m font-medium me-2 px-3.5 py-1.5 rounded-full dark:bg-gray-700 dark:text-gray-300">Famous Quotes</span>
+                    { quote.tags.map((tag, id) => <span key={id} className="bg-gray-300 text-gray-800 text-m font-medium me-2 px-3.5 py-1.5 rounded-full dark:bg-gray-700 dark:text-gray-300">{tag}</span>) }
                 </div>
             </div>
         </section>
         
         <div className="w-full flex">
-            <button type="button" className="px-5 py-3 m-auto text-base font-medium text-center inline-flex items-center text-white bg-stone-600 rounded-lg hover:bg-stone-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button onClick={() => handleSetQuote(null)} type="button" className="px-5 py-3 m-auto text-base font-medium text-center inline-flex items-center text-white bg-stone-600 rounded-lg hover:bg-stone-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <img src={GenerateAgain} className='w-6 h-6 me-2' />
                 Generate another
             </button>
