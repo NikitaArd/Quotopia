68fe0e8ad7f91dbbe4437706fdb10fcb210a73bf
NikitaArd <11nik11255@gmail.com>
2024-04-25 14:29:18 +0200

    feat: filled modal window with information

--- src/components/InfoModal.jsx
+++ src/components/InfoModal.jsx
@@ -12,8 +12,17 @@ export const InfoModal = forwardRef(function Modal(props, ref) {
   return (
     <dialog id="my_modal_1" className="modal" ref={dialog}>
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click the button below to close</p>
+        <h3 className="font-bold text-lg">Hi there ! 👋</h3>
+        <p className="py-4">I hope you will enjoy this application. This is my first personal project with React. I am very excited to immerse myself in React concepts and environment as a software engineer. Thanks for visiting this site.<br/> Have a good day! ☀️</p>
+        <p className="my-4">Also, if you have any advice for improving this project or just want to chat, you can find me there.</p>
+        <div className="w-0.5 m-auto flex justify-around">
+          <a href="https://github.com/NikitaArd" target="blank" type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-2.5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          <svg className="w-10 h-10" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 24 24">
+    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+</svg>
+          </a>
+
+        </div>
         <div className="modal-action">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
